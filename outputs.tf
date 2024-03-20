a32bc2c468322b3a3d94d2c35a7279faaeb00c7a
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-03-19 22:41:59 -0400

    Add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "staging_cluster_issuer" {
-    value = local.create_issuers ? kubectl_manifest.lets_encrypt_staging[0].name : null
+    value       = local.create_issuers ? kubectl_manifest.lets_encrypt_staging[0].name : null
+    description = "staging cluster issuer name for Let's Encrypt if created"
 }
 
 output "cluster_issuer" {
-    value = local.create_issuers ? kubectl_manifest.lets_encrypt[0].name : null
+    value       = local.create_issuers ? kubectl_manifest.lets_encrypt[0].name : null
+    description = "production cluster issuer name for Let's Encrypt if created"
 }
