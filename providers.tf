8de316c1fa46472b348adc756ad05ae84a58ca01
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-08-24 12:08:24 -0400

    Update kubectl provider version

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         kubectl = {
             source  = "gavinbunney/kubectl"
-            version = ">= 1.18.0"
+            version = ">= 1.19.0"
         }
     }
 }
